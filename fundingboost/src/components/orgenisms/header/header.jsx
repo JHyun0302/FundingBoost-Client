7c45aad9c380c38bff7fa5e9c7e5c353264c15da
SemiHyeon <gustpal08@gmail.com>
2024-04-13 21:34:35 +0900

    header 클래스명 수정

--- fundingboost/src/components/orgenisms/header/header.jsx
+++ fundingboost/src/components/orgenisms/header/header.jsx
@@ -15,13 +15,13 @@ function HeaderBar() {
                 <Navbar.Brand href="/" className="navbar-logo"><img src ={logoImg}/></Navbar.Brand>
                 <Navbar.Toggle aria-controls="navbarScroll" />
                 <Navbar.Collapse id="navbarScroll">
-                    <Nav className="navbarmemu" navbarScroll>
+                    <Nav className="navbarMemu" navbarScroll>
                         <Nav.Link href="/home" className="home-btn" >홈</Nav.Link>
                         <Nav.Link href="/friendfunding" className="friend-btn">친구펀딩</Nav.Link>
                         <Nav.Link href="/shopping" className="shopping-btn">쇼핑하기</Nav.Link>
                         <Nav.Link href="/mypage" className="my-btn">MY</Nav.Link>
                     </Nav>
-                    <Form className="seachbar">
+                    <Form className="seachBar">
                         <Form.Control type="search" placeholder="상품을 검색해보세요"
                                       className="me-2"
                                       aria-label="Search"
