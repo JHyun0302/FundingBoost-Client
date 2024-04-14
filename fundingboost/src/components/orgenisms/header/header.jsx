a27b1409283a39f11f6fbc9f7c3ec8b22130053c
SemiHyeon <gustpal08@gmail.com>
2024-04-14 16:44:17 +0900

    header 로그인에 따른 상태 변화 추가

--- fundingboost/src/components/orgenisms/header/header.jsx
+++ fundingboost/src/components/orgenisms/header/header.jsx
@@ -1,14 +1,28 @@
-import { Button, Container, Form, Nav, Navbar, NavDropdown } from 'react-bootstrap';
 import React, { useState } from 'react';
+import { Button, Container, Form, Nav, Navbar, NavDropdown } from 'react-bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './header.scss';
 
-import  logoImg from '../../../assets/logo.svg'
-import gifthub from '../../../assets/gifthub.svg'
-// import { CiSearch } from "react-icons/ci";
+import  logoImg from '../../../assets/logo.svg';
+import gifthub from '../../../assets/gifthub.svg';
+//import { CiSearch } from "react-icons/ci";
 
 //헤더 네이게이션바
 function HeaderBar() {
+    const [isLoggedIn, setIsLoggedIn] = useState(false);
+    const [nickName, setNickName] = useState('');
+
+    // 로그인
+    const login = () => {
+        setIsLoggedIn(true);
+        setNickName('nick_name');
+    };
+    // 로그아웃
+    const logout = () => {
+        setIsLoggedIn(false);
+        setNickName("");
+    };
+
     return (
         <Navbar expand="lg" className="headerBar">
             <Container fluid>
@@ -16,7 +30,7 @@ function HeaderBar() {
                 <Navbar.Toggle aria-controls="navbarScroll" />
                 <Navbar.Collapse id="navbarScroll">
                     <Nav className="navbarMemu" navbarScroll>
-                        <Nav.Link href="/home" className="home-btn" >홈</Nav.Link>
+                        <Nav.Link href="/home" className="home-btn">홈</Nav.Link>
                         <Nav.Link href="/friendfunding" className="friend-btn">친구펀딩</Nav.Link>
                         <Nav.Link href="/shopping" className="shopping-btn">쇼핑하기</Nav.Link>
                         <Nav.Link href="/mypage" className="my-btn">MY</Nav.Link>
@@ -25,15 +39,23 @@ function HeaderBar() {
                         <Form.Control type="search" placeholder="상품을 검색해보세요"
                                       className="me-2"
                                       aria-label="Search"
-
                         />
                         <Button className="gifthub-btn" href="/gifthub"><img src={gifthub}/></Button>
-                        <div className="login-btn">
-                            <NavDropdown title="로그인" id="navbarScrollingDropdown">
-                                <NavDropdown.Item href="#action3">로그아웃</NavDropdown.Item>
-                            </NavDropdown>
-                        </div>
                     </Form>
+
+                    {/*로그인 여부에따른 변화*/}
+                    <div  className="loginLogout">
+                        {isLoggedIn ? (
+                            <NavDropdown title={nickName} id="logoutDropdown" drop="down">
+                                <NavDropdown.Item onClick={logout}>로그아웃</NavDropdown.Item>
+                            </NavDropdown>
+
+                        ) : (
+                            <button onClick={login} className="login-btn" >로그인</button>
+
+                        )}
+                    </div>
+
                 </Navbar.Collapse>
             </Container>
         </Navbar>
