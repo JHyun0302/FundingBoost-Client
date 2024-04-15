9273bffa5da7dfe25c70e19101defad2e7241d8f
HyeIn <helen6662@naver.com>
2024-04-15 10:24:51 +0900

    [feat] : add footer

--- fundingboost/src/App.js
+++ fundingboost/src/App.js
@@ -1,25 +1,16 @@
+import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import '@fortawesome/fontawesome-free/css/all.min.css';
+import 'mdb-react-ui-kit/dist/css/mdb.min.css';
+import Footer from './components/organisms/footer/footer'
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    return (
+        <div className="App">
+                <Footer />
+        </div>
+    );
 }
 
 export default App;
