176ca3401fa8c7a416d228f9a15d3b9441068ed9
HyeIn <helen6662@naver.com>
2024-04-17 11:12:01 +0900

    [feat] : add ranking item's ballon area

---
+++ fundingboost/src/components/molecules/RankingItemPane/rankingitempane.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import './rankingitempane.scss';
+
+const MainPane = () => {
+    return (
+        <div className="ranking-item-pane-container">
+            <div class="balloon">
+
+            </div>
+        </div>
+    );
+}
+
+export default MainPane;
