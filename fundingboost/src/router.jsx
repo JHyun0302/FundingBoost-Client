5decc358a67c0dccdd4fac016172c5467c7102f0
HyeIn <helen6662@naver.com>
2024-04-23 16:26:04 +0900

    [feat] : 코드 스플리팅 적용

--- fundingboost/src/router.jsx
+++ fundingboost/src/router.jsx
@@ -1,19 +1,25 @@
-import { BrowserRouter, Outlet, Route, Routes } from "react-router-dom";
-import Main from "../src/components/pages/main-page/main-page"
-import Login from "../src/components/pages/login-page/login-page"
-import Gifthub from "./components/pages/gifthub-page/gifthub-page";
-import Mypage from "./components/pages/mypage-page/mypage-page"
+import React, { lazy, Suspense } from 'react';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 
-export default function Router() {
+// 동적으로 로드할 컴포넌트를 lazy 함수를 사용하여 import
+const Main = lazy(() => import('../src/components/pages/main-page/main-page'));
+const Login = lazy(() => import('../src/components/pages/login-page/login-page'));
+const Gifthub = lazy(() => import('./components/pages/gifthub-page/gifthub-page'));
+const Mypage = lazy(() => import('./components/pages/mypage-page/mypage-page'));
+
+// Suspense 컴포넌트를 사용하여 로딩 중일 때 보여줄 UI를 지정
+function Router() {
     return (
         <BrowserRouter>
             <Routes>
-                <Route path="/" element={<Main />}/>
-                <Route path="/home" element={<Main />}/>
-                <Route path="/login" element={<Login />} />
-                <Route path="/api/v1/gifthub" element={<Gifthub />} />
-                <Route path="/api/v1/mypage" element={<Mypage />} />
+                <Route path="/" element={<Suspense fallback={<div>Loading FundingBoost...</div>}><Main /></Suspense>} />
+                <Route path="/home" element={<Suspense fallback={<div>Loading FundingBoost...</div>}><Main /></Suspense>} />
+                <Route path="/login" element={<Suspense fallback={<div>Loading FundingBoost...</div>}><Login /></Suspense>} />
+                <Route path="/gifthub" element={<Suspense fallback={<div>Loading FundingBoost...</div>}><Gifthub /></Suspense>} />
+                <Route path="/mypage" element={<Suspense fallback={<div>Loading FundingBoost...</div>}><Mypage /></Suspense>} />
             </Routes>
         </BrowserRouter>
     );
 }
+
+export default Router;
