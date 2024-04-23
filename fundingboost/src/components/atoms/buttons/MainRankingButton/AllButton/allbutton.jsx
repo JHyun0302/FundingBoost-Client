918c6888c395194d56f17339ea17077d4054b0ef
HyeIn <helen6662@naver.com>
2024-04-23 12:05:59 +0900

    [feat] : Main 페이지 상품 랭킹 카테고리 버튼 클릭 이벤트 추가 및 코드 리팩토링

--- fundingboost/src/components/atoms/buttons/MainRankingButton/AllButton/allbutton.jsx
+++ fundingboost/src/components/atoms/buttons/MainRankingButton/AllButton/allbutton.jsx
@@ -1,16 +1,18 @@
-import React from 'react';
+import React, { useState } from 'react';
 import '../AllButton/allbutton.scss';
 import allRankingIcon from '../../../../../assets/ranking/allranking.png';
 
-const AllButton = () => {
+const AllButton = ({ isSelected, onClick }) => {
     return (
         <div className="button-container">
-            <button className="circle-button">
+            <button
+                className={`circle-button ${isSelected ? 'selected' : ''}`} // isSelected prop을 사용하여 선택 여부를 확인하고 클래스를 추가
+                onClick={onClick} // 클릭 이벤트 핸들러를 전달
+            >
                 <img src={allRankingIcon} alt="All Ranking" className="button-icon" />
                 <p className="button-label">모두가</p>
             </button>
         </div>
     );
 }
-
 export default AllButton;
