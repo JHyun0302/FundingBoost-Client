30984ec13d6aa502da1304cea0699bf2a1b02a0c
SemiHyeon <gustpal08@gmail.com>
2024-05-06 20:14:45 +0900

    [feat] : main friend funding 캐러셀 기능 추가

--- fundingboost/src/components/molecules/mainFriendFunding/mainFriendFunding/mainFriendFunding.jsx
+++ fundingboost/src/components/molecules/mainFriendFunding/mainFriendFunding/mainFriendFunding.jsx
@@ -2,13 +2,20 @@ import React from 'react';
 import './mainFriendFunding.scss';
 import ItemImg from "../../../atoms/itemImg/itemImg";
 import GaugeBar from "../../../atoms/gauge-bar/gauge-bar";
-import { Carousel } from 'react-responsive-carousel';
 import ProfileImg from "../../../atoms/ProfileImg/ProfileImg";
 import defaultProfileImg from "../../../../assets/defaultProfile.svg";
-import {useEffect} from "react";
+import { useEffect, useState } from "react";
+import { Carousel } from 'react-responsive-carousel';
+
+function chunkArray(arr, chunkSize) {
+    const chunkedArray = [];
+    for (let i = 0; i < arr.length; i += chunkSize) {
+        chunkedArray.push(arr.slice(i, i + chunkSize));
+    }
+    return chunkedArray;
+}
 
 const MainFriendFunding = ({ memberFundingData }) => {
-
     useEffect(() => {
         console.log("친구펀딩 데이터:", memberFundingData);
     }, [memberFundingData]);
@@ -23,67 +30,47 @@ const MainFriendFunding = ({ memberFundingData }) => {
 
             <div className="friendFunding-contents">
                 {friendFundingData && friendFundingData.length > 0 && (
-                    <div className="friendFunding-items-container">
-                        {friendFundingData.map((friendFunding, index) => (
-                        <div key={index} className="mainFriendFundingitem">
-                            <div className="mainFriendFundingContents">
-                                <div className="mainFriendFundingProfile-item">
-                                    <ItemImg imageUrl={friendFunding.commonFriendFundingDto.friendFundingPageItemDtoList[0].itemImageUrl} className="mainFriendFunding-Itemimg"/>
-                                    <img src={friendFunding.commonFriendFundingDto.friendProfileImgUrl || defaultProfileImg} alt="프로필 이미지" className="mainFriendFunding-profile-img"/>
-                                </div>
-                                <div className="mainFriendFunding-textitem-GaugeBar">
-                                    <div className="mainFriendFunding-textContents">
-                                        <div className="mainFriendFunding-NickName-Dday">
-                                            <div className="mainFriendFunding-NickName">{friendFunding.commonFriendFundingDto.nickName}</div>
-                                            <div className="mainFriendFunding-D-day">{friendFunding.commonFriendFundingDto.friendFundingDeadlineDate} </div>
+                    <Carousel
+                        showArrows={true}
+                        showThumbs={false}
+                        showStatus={false}
+                        showIndicators={true}
+                        emulateTouch={true}
+                        arrows={false}
+                        // autoPlay={true}
+                        // interval={2000}
+                        infinite={true}
+                    >
+                        {chunkArray(friendFundingData, 5).map((chunk, index) => (
+                            <div key={index} className="mainFriendFundingitem">
+                                {chunk.map((friendFunding, idx) => (
+                                    <div key={idx} className="mainFriendFundingContents">
+                                        <div className="mainFriendFundingProfile-item">
+                                            <ItemImg imageUrl={friendFunding.commonFriendFundingDto.friendFundingPageItemDtoList[0].itemImageUrl} className="mainFriendFunding-Itemimg"/>
+                                            <img src={friendFunding.commonFriendFundingDto.friendProfileImgUrl || defaultProfileImg} alt="프로필 이미지" className="mainFriendFunding-profile-img"/>
                                         </div>
-                                        <div>
-                                            <div className="mainFriendFunding-Tag">{friendFunding.commonFriendFundingDto.tag}</div>
+                                        <div className="mainFriendFunding-textitem-GaugeBar">
+                                            <div className="mainFriendFunding-textContents">
+                                                <div className="mainFriendFunding-NickName-Dday">
+                                                    <div className="mainFriendFunding-NickName">{friendFunding.commonFriendFundingDto.nickName}</div>
+                                                    <div className="mainFriendFunding-D-day">{friendFunding.commonFriendFundingDto.friendFundingDeadlineDate} </div>
+                                                </div>
+                                                <div>
+                                                    <div className="mainFriendFunding-Tag">{friendFunding.commonFriendFundingDto.tag}</div>
+                                                </div>
+                                            </div>
+                                            <div className="mainFriendFunding-GaugeBarContent">
+                                                <GaugeBar value={friendFunding.commonFriendFundingDto.friendFundingPercent} className="mainFriendFunding-GaugeBar"/>
+                                            </div>
                                         </div>
-
                                     </div>
-                                    <div className="mainFriendFunding-GaugeBarContent">
-                                        <GaugeBar value={friendFunding.commonFriendFundingDto.friendFundingPercent} className="mainFriendFunding-GaugeBar"/>
-                                    </div>
-                                </div>
+                                ))}
                             </div>
-                        </div>
-                    ))}
-                        </div>
+                        ))}
+                    </Carousel>
                 )}
             </div>
         </div>
-        // {/*<div className ="friendFunding-contents">*/}
-        //         {/*<Carousel*/}
-        //         {/*    showArrows={true}*/}
-        //         {/*    showThumbs={false}*/}
-        //         {/*    showStatus={false}*/}
-        //         {/*    showIndicators={false}*/}
-        //         {/*    emulateTouch={true}*/}
-        //         {/*    arrows ={false}*/}
-        //         {/*>*/}
-        // {/*            {memberFundingData?.data?.homeFriendFundingDtoList?.map((product, index) => (*/}
-        // {/*                <div className="myFundingItem-a">*/}
-        // {/*                    <div className="mainFriendFundingContents" key={index}>*/}
-        // {/*                        <div className ="mainFriendFundingProfile-item">*/}
-        // {/*                            <ItemImg imageUrl={product.friendProfileImgUrl} className="myFundingItemimg"/>*/}
-        // {/*                            <img src={product.profile || defaultProfileImg}*/}
-        // {/*                                 alt="프로필 이미지" className="profile-img"/>*/}
-        // {/*                        </div>*/}
-        // {/*                        <div>*/}
-        // {/*                            <div>*/}
-        // {/*                                <div className="myfundingNickName">{product.nickName}님</div>*/}
-        // {/*                                <div className="memberFundingD-day">{product.friendFundingDeadlineDate} 종료일</div>*/}
-        // {/*                                <div className="mainFriendFundingTag">{product.tag}태그</div>*/}
-        // {/*                            </div>*/}
-        // {/*                            <GaugeBar value={product.friendFundingPercent} className="myFundingGaugeBar"/>*/}
-        // {/*                        </div>*/}
-        // {/*                    </div>*/}
-        // {/*                </div>*/}
-        // {/*            ))}*/}
-        // {/*        /!*</Carousel>*!/*/}
-        // {/*    </div>*/}
-        // {/*</div>*/}
     );
 };
 
