b5738a424d4560c34a4013cfe712637a043bccad
SemiHyeon <gustpal08@gmail.com>
2024-05-07 02:43:12 +0900

    [style] : 메인페이지 수정

--- fundingboost/src/components/pages/main-page/main-page.jsx
+++ fundingboost/src/components/pages/main-page/main-page.jsx
@@ -7,15 +7,14 @@ import '@fortawesome/fontawesome-free/css/all.min.css';
 import 'mdb-react-ui-kit/dist/css/mdb.min.css';
 import Footer from '../../organisms/footer/footer'
 import MainPane from '../../organisms/contents/main/main'
-import MainMyfunding from "../../molecules/mainMyfunding/mainMyfunding/mainMyfunding";
-import MainFriendNoFunding  from "../../molecules/mainFriendFunding/mainFriendNoFunding";
-
+import MainFunding from "../../molecules/mainFunding/mainFunding";
+import MainFriendFunding from "../../molecules/mainFriendFunding/mainFriendFunding/mainFriendFunding";
+import MainFriendNoFunding from "../../molecules/mainFriendFunding/mainFriendNoFunding/mainFriendNoFunding";
 function MainPage() {
     return (
         <div className="Main-pages">
             <HeaderBar />
-            <MainMyfunding/>
-            <MainFriendNoFunding/>
+            <MainFunding/>
             <MainPane />
             <Footer />
         </div>
