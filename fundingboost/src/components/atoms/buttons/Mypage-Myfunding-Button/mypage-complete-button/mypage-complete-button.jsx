e0b18e247fdc03139f9649c78108801690c85e1c
HyeIn <helen6662@naver.com>
2024-05-07 14:31:21 +0900

    [fix] : fix bug

---
+++ fundingboost/src/components/atoms/buttons/Mypage-Myfunding-Button/mypage-complete-button/mypage-complete-button.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+import './mypage-complete-button.scss';
+
+function MypageCompleteBtn() {
+    return (
+        <button className="mypage-complete-btn">완료</button>
+    );
+}
+
+export default MypageCompleteBtn;
