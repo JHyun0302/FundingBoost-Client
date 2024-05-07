0f064bd5d5dd630cef25ff48eedb18a2d8e74738
SemiHyeon <gustpal08@gmail.com>
2024-05-08 00:26:21 +0900

    [feat] : 퍼센트 데이터 통신, 캐러셀에 대한 css 수정

--- fundingboost/src/components/molecules/mainMyfunding/memberYesFunding/memberYesFunding.jsx
+++ fundingboost/src/components/molecules/mainMyfunding/memberYesFunding/memberYesFunding.jsx
@@ -4,22 +4,17 @@ import React, { useState, useEffect } from "react";
 import ItemImg from "../../../atoms/itemImg/itemImg";
 import GaugeBar from "../../../atoms/gauge-bar/gauge-bar";
 import { Carousel } from 'react-responsive-carousel';
-
 import 'react-responsive-carousel/lib/styles/carousel.min.css';
 import './memberYesFunding.scss';
 import axios from "axios";
 
 function MemberYesFunding({ memberFundingData }) {
     const [isMobile, setIsMobile] = useState(window.innerWidth <= 970);
-    const [products, setProducts] = useState([]);
 
-    // const [fundingDeadline, setFundingDeadline] = useState('');
-    // const [myFundingItems, setMyFundingItems] = useState([]);
-
-
+    //반응형 추가
     useEffect(() => {
         const handleResize = () => {
-            setIsMobile(window.innerWidth <= 970);
+            setIsMobile(window.innerWidth <= 1085);
         };
         handleResize();
         window.addEventListener('resize', handleResize);
@@ -29,74 +24,77 @@ function MemberYesFunding({ memberFundingData }) {
         };
     }, []);
 
-    console.log("상품이미지:",memberFundingData?.data?.homeMyFundingItemDtoList?.itemImageUrl);
+    console.log("상품이미지:", memberFundingData?.data?.homeMyFundingItemDtoList?.itemImageUrl);
 
+    function chunkArray(arr, chunkSize) {
+        const chunkedArray = [];
+        for (let i = 0; i < arr.length; i += chunkSize) {
+            chunkedArray.push(arr.slice(i, i + chunkSize));
+        }
+        return chunkedArray;
+    }
 
     return (
         <div className="memberYesFunding">
-                <div className="memberYesFundingstatus">
-
-                    <div className="memberYesFunding-item">
-
-                        <ProfileImg className="memberYesFunding-Profile" memberFundingData={memberFundingData.data} />
+            <div className="memberYesFundingstatus">
+                <div className="memberYesFunding-item">
+                    <ProfileImg className="memberYesFunding-Profile" memberFundingData={memberFundingData.data} />
+                    <div className="memberYesFunding-text">
                         <div className="memberYesFunding-text">
-                            <div className="memberYesFunding-text">
-                                <div className="myfundingNickName">{memberFundingData?.data?.homeMemberInfoDto?.nickName}님</div>
-                                펀딩 현황
-                            </div>
-                            <div className="memberFundingD-day">{memberFundingData?.data?.homeMyFundingStatusDto?.deadline}</div>
-
+                            <div className="myfundingNickName">{memberFundingData?.data?.homeMemberInfoDto?.nickName}님</div>
+                            펀딩 현황
                         </div>
-                        <div className="memberFunding-RightItem">
-                            <div className="memberFundingProgress">%</div>
-                            <CheckFundingButton/>
-                        </div>
+                        <div className="memberFundingD-day">{memberFundingData?.data?.homeMyFundingStatusDto?.deadline}</div>
+                    </div>
+                    <div className="memberFunding-RightItem">
+                        <div className="memberFundingProgress">{memberFundingData?.data?.homeMyFundingStatusDto?.totalPercent}%</div>
+                        <CheckFundingButton/>
+                    </div>
                 </div>
 
-                    {/* 화면 사이즈 768보자 작은 경우 Carousel추가*/}
-                    {!isMobile? (
-                        <div className="myFundingItemsContainer mobile-carousel">
-                            <div className="myFundingItems">
+                {/* 반응형 Carousel 추가 */}
+                {!isMobile ? (
+                    <div className="myFundingItemsContainer mobile-carousel">
+                        <div className="myFundingItems">
                             {memberFundingData?.data?.homeMyFundingStatusDto?.homeMyFundingItemDtoList?.map((product, index) => (
-                                <div className="myFundingItem-a">
-                                    <div className="myFundingItem" key={index}>
-                                        <ItemImg imageUrl={product.itemImageUrl} className="myFundingItemimg"/>
+                                <div className="myFundingItem-a" key={index}>
+                                    <div className="myFundingItem">
+                                        <ItemImg imageUrl={product.itemImageUrl} className="myFundingItemimg" />
                                         <GaugeBar value={product.itemPercent} className="myFundingGaugeBar"/>
                                     </div>
                                 </div>
                             ))}
-                            </div>
                         </div>
-                    ): (
-                        <div className="myFundingItemsContainer mobile-carousel">
-                            <div className="myFundingItems">
-                                <Carousel
-                                    showArrows={true}
-                                    showThumbs={false}
-                                    showStatus={false}
-                                    showIndicators={false}
-                                    emulateTouch={true}
-
-                                    arrows ={false}
-                                    autoPlay={true} //자동슬라이드
-                                    interval={2000}
-                                    infinite={true} //무한 슬라이드
-                                >
-                                    {memberFundingData?.data?.homeMyFundingStatusDto?.homeMyFundingItemDtoList?.map((product, index) => (
-                                        <div className="myFundingItem-a">
+                    </div>
+                ) : (
+                    <div className="myFundingItemsContainer mobile-carousel">
+                        <div className="myFundingItems">
+                            <Carousel
+                                showArrows={true}
+                                showThumbs={false}
+                                showStatus={false}
+                                showIndicators={false}
+                                emulateTouch={true}
+                                arrows={false}
+                                autoPlay={true}
+                                interval={2000}
+                                infinite={true}
+                            >
+                                {chunkArray(memberFundingData?.data?.homeMyFundingStatusDto?.homeMyFundingItemDtoList, 3).map((chunk, index) => (
+                                    <div className="myFundingItem-a" key={index}>
+                                        {chunk.map((product, index) => (
                                             <div className="myFundingItem" key={index}>
                                                 <ItemImg imageUrl={product.itemImageUrl} className="myFundingItemimg" />
                                                 <GaugeBar value={product.itemPercent} className="myFundingGaugeBar"/>
                                             </div>
-                                        </div>
-                                    ))}
-
-                        </Carousel>
-                            </div>
-
+                                        ))}
+                                    </div>
+                                ))}
+                            </Carousel>
                         </div>
-                    )}
-                </div>
+                    </div>
+                )}
+            </div>
         </div>
     );
 }
