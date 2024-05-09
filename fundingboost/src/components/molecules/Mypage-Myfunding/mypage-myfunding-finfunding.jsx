8b077ce85a8305618259b9956b95e90eea6ae02a
HyeIn <helen6662@naver.com>
2024-05-09 11:01:22 +0900

    [feat] : Mypage My펀딩 종료 버튼 조건부 렌더링

--- fundingboost/src/components/molecules/Mypage-Myfunding/mypage-myfunding-finfunding.jsx
+++ fundingboost/src/components/molecules/Mypage-Myfunding/mypage-myfunding-finfunding.jsx
@@ -1,35 +1,61 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import './mypage-myfunding-dofunding.scss';
 import shareicon from "../../../assets/share.svg";
 import MyfundingAdditionalPane from "../../atoms/mypage-additional-info/mypage-additional-info";
 import MyfundingItemList from "../mypage-myfunding-itemlist/mypage-myfunding-itemlist";
 import messagebox from "../../../assets/messagebox.svg";
+import MyPageMyFundingMessage from "../Modal/MypageMyfundingMessage/mypagemyfundingmessage"
+import messageboxopen from "../../../assets/messagebox-open.svg"
 
-const MyfundingFinFundingPane = ({ deadline, totalPercent, message, tag, participateFriendDtoList, myPageFundingItemDtoList }) => {
+const MyfundingFinFundingPane = ({ apiData, deadline, deadlineDate, totalPercent, message, tag, participateFriendDtoList, myPageFundingItemDtoList}) => {
+    const [showModal, setShowModal] = useState(false); // 모달 열림 여부 상태 관리
+    const [isHovered, setIsHovered] = useState(false);
+
+    const handleHover = () => {
+        setIsHovered(!isHovered);
+    };
+
+    const toggleModal = () => {
+        setShowModal(!showModal);
+    };
+
+
+
     return (
         <div className="mypage-right-pane-containter">
             <div className="mypage-myfunding-content-wrapper">
                 <div className="mypage-myfunding-title-wrapper">
                     <div className="mypage-day-wrpper">
                         <div className="mypage-myfunding-title">MY 펀딩</div>
-                        <div className="memberFundingDday"> 종료</div>
+                        <div className="memberFundingDday">펀딩이 {deadlineDate}되었습니다.</div>
                         <div className="mypage-enddate-pane">~ {deadline}</div>
                         <div className="mypage-tag-pane">{tag}</div>
-
+                        <button
+                            className="mypage-message-btn"
+                            onClick={toggleModal}
+                            onMouseEnter={handleHover}
+                            onMouseLeave={handleHover}
+                        >
+                            <img
+                                src={isHovered ? messageboxopen : messagebox}
+                                alt="messagebox"
+                                className="mypage-message-icon"
+                            />
+                        </button>
                     </div>
                     <div className="mypage-fin-button-noti-wrpper">
-                            ＊ 펀딩이 종료되었습니다.
+                        ＊ 펀딩이 종료되었습니다.
                         <button className="mypage-button-style-01">
                             <img src={shareicon} alt="shareicon" className="mypage-share-icon"/>
                         </button>
                     </div>
                 </div>
                 <div className="horizontalLine"></div>
-                <MyfundingItemList myPageFundingItemDtoList={myPageFundingItemDtoList} />
+                <MyfundingItemList myPageFundingItemDtoList={myPageFundingItemDtoList} apiData={apiData}/>
                 <div className="horizontalLine"></div>
                 <MyfundingAdditionalPane participateFriendDtoList={participateFriendDtoList} totalPercent={totalPercent}/>
-
             </div>
+            <MyPageMyFundingMessage show={showModal} message={message} handleClose={toggleModal} />
         </div>
     );
 }
