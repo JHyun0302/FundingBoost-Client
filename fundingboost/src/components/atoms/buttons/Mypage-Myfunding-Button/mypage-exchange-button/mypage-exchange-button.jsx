4c6617540cd5ec92f61caaace153e749b40d6c5b
HyeIn <helen6662@naver.com>
2024-05-09 11:02:01 +0900

    [feat] : Mypage My펀딩 포인트 전환, 잔여금액 결제 버튼

---
+++ fundingboost/src/components/atoms/buttons/Mypage-Myfunding-Button/mypage-exchange-button/mypage-exchange-button.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+import './mypage-exchange-button.scss';
+
+function MypageExchangeBtn() {
+    return (
+        <button className="mypage-exchange-btn">포인트 전환하기</button>
+    );
+}
+
+export default MypageExchangeBtn;
