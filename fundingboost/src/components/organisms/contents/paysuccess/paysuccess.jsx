386f67134e11a2540aa2fb5c0a971c82aa789e78
HyeIn <helen6662@naver.com>
2024-05-10 14:21:52 +0900

    [feat] : 결제완료 페이지 구성

--- fundingboost/src/components/organisms/contents/paysuccess/paysuccess.jsx
+++ fundingboost/src/components/organisms/contents/paysuccess/paysuccess.jsx
@@ -1,13 +1,19 @@
 import React from 'react';
 import '../paysuccess/paysuccess.scss';
-
+import Paysuccess_icon from '../../../../assets/pay-success.svg';
+import PaySuccessBtn from "../../../atoms/buttons/PaysuccessButton/paysuccessubutton";
 
 const PaySuccessPane = () => {
     return (
         <div className="success-pane-container">
-
+            <div className="success-horizontalLine-up"></div>
+            <img src={Paysuccess_icon} alt="Pay Success" className="success-icon" />
+            <div className="pay-success-text">
+                💰 결제가 완료되었습니다 💰
+            </div>
+            <PaySuccessBtn/>
+            <div className="success-horizontalLine-down"></div>
         </div>
-
     );
 }
 
