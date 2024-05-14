6427bc1a1700fcdf9ebbaf6fe633e7fb26ed915b
HyeIn <helen6662@naver.com>
2024-05-14 16:15:19 +0900

    [feat] : Main 페이지 상품 통신 및 렌더링

--- fundingboost/src/components/molecules/MainRankingList/mainrankingitem.jsx
+++ fundingboost/src/components/molecules/MainRankingList/mainrankingitem.jsx
@@ -3,35 +3,40 @@ import style from './mainrankingitem.scss';
 import { Link } from 'react-router-dom';
 import { formatPrice } from '../../../utils/formats';
 
-export default function Mainrankingitem({ products }) {
-    // products가 undefined인 경우를 처리
-    if (!products) {
+export default function Mainrankingitem({ product }) {
+
+    if (!product) {
         return null;
     }
 
-    const price = products.price || 10000; // 기본값으로 10000 설정
+    const { itemId, itemName, price, itemImageUrl, brandName } = product;
 
-    const truncatedTitle = products.title.length > 25 ? products.title.slice(0, 25) + '...' : products.title;
+    console.log(product)
+    
+    // 상품 타이틀이 너무 길 경우 일부만 표시
+    const truncatedTitle = itemName.length > 25 ? itemName.slice(0, 25) + '...' : itemName;
 
     return (
-        <Link to={"/products/" + products.id} className={style.products} style={{ textDecoration: 'none' }}>
+        <Link to={"/product/" + itemId} className={style.products} style={{textDecoration: 'none'}}>
             <div className={style.imgwrap}>
-                {products.isSoldOut ?
-                    <div className={style.soldout}>
-                        <p className={style.text}>Sold Out</p>
-                        <div className={style.bg}></div>
-                    </div>
-                    : null}
-                <div className="img-wrapper">
-                    <img src={products.thumbnail} width="100%" style={{ borderRadius: '3px' }} alt={products.title}/>
+                {/* 상품 품절 여부에 따라 표시 */}
+                <div className={style.soldout}>
+                    {/*<p className={style.text}>{price > 0 ? "판매중" : "Sold Out"}</p>*/}
+                    <div className={style.bg}></div>
                 </div>
+                <div className="img-wrapper">
+                    <img src={itemImageUrl} width="100%" style={{borderRadius: '3px'}} alt={itemName}/>
+                </div>
+            </div>
+            <div className="brand-wrapper">
+                <p className={style.brand}>{brandName}</p>
             </div>
             <div className="title-wrapper">
                 <h4 className={style.title}>{truncatedTitle}</h4>
             </div>
             <div className="price-wrapper">
-                <p className={style.price}>{formatPrice(price)}원</p>
+                <p className={style.price}>{price}원</p>
             </div>
         </Link>
-    )
+    );
 }
