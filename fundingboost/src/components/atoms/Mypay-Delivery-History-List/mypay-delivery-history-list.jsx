db7290278ddb298e8b35a0813f2db702b49f7261
HyeIn <helen6662@naver.com>
2024-05-14 14:13:09 +0900

    [feat] : 잔여 금액 결제 페이지 배송지 입력 및 내역 자동화

---
+++ fundingboost/src/components/atoms/Mypay-Delivery-History-List/mypay-delivery-history-list.jsx
@@ -0,0 +1,22 @@
+import React from "react";
+import "./mypay-delivery-history-list.scss";
+import MypayDeliveryHistory from "../Mypay-Delivery-History/mypay-delivery-history";
+
+export default function MypayDeliveryHistoryList({ deliveryDtoList, onSelectItem }) {
+    if (!deliveryDtoList || deliveryDtoList.length === 0) {
+        return <div>No delivery history</div>;
+    }
+
+    return (
+        <div className="MyPayDeliveryListBox">
+            {deliveryDtoList.map((delivery, index) => (
+                <div key={index} className="MyPayDeliveryListGroup">
+                    <div className="MyPayDeliveryListView-wrapper">
+                        <MypayDeliveryHistory key={index} delivery={delivery} onSelectItem={onSelectItem}/>
+                    </div>
+                </div>
+            ))}
+        </div>
+    );
+};
+
