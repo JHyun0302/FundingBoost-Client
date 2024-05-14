44a31122c7a1f5ba8fa9414423ec1196594df614
HyeIn <helen6662@naver.com>
2024-05-14 14:13:33 +0900

    [feat] : 잔여 금액 결제 페이지 상품 및 결제 금액 렌더링, 계산 로직 추가

---
+++ fundingboost/src/components/molecules/Mypay-Product-Detail/mypay-product-detail.jsx
@@ -0,0 +1,34 @@
+import React from 'react';
+import './mypay-product-detail.scss';
+
+const MypayProductDetail = ({ myPageFundingItemDtoList }) => {
+
+    console.log("Item:", myPageFundingItemDtoList);
+
+        const item = myPageFundingItemDtoList;
+
+        return (
+            <div>
+                <div className="mypay-product-details-text">상품내역</div>
+                <div className="MyPayItemBox">
+                    <div className="MyPayItemOverlap">
+                        <div className="MyPayItem-left-group">
+                            <img className="MyPayItem-image" alt="ItemImage" src={item.itemImageUrl}/>
+                            <div className="name-option-group">
+                                <p className="MyPayItem-item-name">{item.itemName}</p>
+                                <div className="MyPayItem-option-group">
+                                    <div className="MyPayItem-option-box">
+                                        <div className="MyPayItem-option">옵션</div>
+                                    </div>
+                                    <div className="MyPayItem-option-name">{item.optionName}</div>
+                                </div>
+                            </div>
+                        </div>
+                        <div className="MyPayItem-price">{item.itemPrice.toLocaleString()} 원</div>
+                    </div>
+                </div>
+            </div>
+        );
+};
+
+export default MypayProductDetail;
