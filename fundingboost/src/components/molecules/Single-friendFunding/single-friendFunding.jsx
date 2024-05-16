36247ed1ca57ff58b21790dfb6ae0ebdf9ebfd3f
SemiHyeon <106103995+SemiHyeon@users.noreply.github.com>
2024-05-16 19:56:24 +0900

    [feat]: 친구펀딩상세 페이지와 연결

--- fundingboost/src/components/molecules/Single-friendFunding/single-friendFunding.jsx
+++ fundingboost/src/components/molecules/Single-friendFunding/single-friendFunding.jsx
@@ -1,37 +1,45 @@
 import React from 'react';
 import './single-friendFunding.scss'
+import { Link } from 'react-router-dom';
 import img from '../../../assets/logo.svg'
 import FriendFundingProfileDday from "../../atoms/friendFunding-profile-Dday/friendFunding-profile-Dday";
 import FriendFundingItemImg from "../../atoms/friendFunding-itemImg/friendFunding-itemImg";
 import GaugeBar from "../../atoms/gauge-bar/gauge-bar";
 
 const SingleFriendFunding = ({friendFundingData}) => {
-
+console.log(friendFundingData);
     return (
 
         <div className="friendFunding">
             <div className="friendFunding-row">
                 <div className="friendFunding-itemlist">
-                        {friendFundingData?.data?.map((fundingData, index) => (
-                                <div className="friendFunding-Background">
-                                    <div className="friendFunding-Background-left">
-                                        <div key={index} className="friendFundingDetail">
-                                            <FriendFundingProfileDday friendFundingData={fundingData}/>
-                                            <FriendFundingItemImg friendFundingData={fundingData}/>
-                                            <div className="friendFunding-gaugeBar">
-                                                <GaugeBar value={fundingData.friendFundingPercent}/>
-                                            </div>
-                                        </div>
-                                    </div>
-                                    <div className="friendFunding-Background-right">
-                                        <div className="friendFundingbtn"></div>
-                                    </div>
-                            </div>
-                                ))}
+                    {friendFundingData?.data?.map((fundingData, index) => (
+                        <Link  key={index}
+                              to={{
+                                  pathname: `/friend-Funding/Detail/${fundingData.fundingId}`,
+                                  state: { fundingId: fundingData.fundingId } }}
+                              className="friendFunding-Background"
+                        >
 
+                            <div className="friendFunding-Background-left">
+                                <div key={index} className="friendFundingDetail">
+                                    <FriendFundingProfileDday friendFundingData={fundingData}/>
+                                    <FriendFundingItemImg friendFundingData={fundingData}/>
+                                    <div className="friendFunding-gaugeBar">
+                                        <GaugeBar value={fundingData.friendFundingPercent}/>
+                                    </div>
+                                </div>
+                            </div>
+                            <div className="friendFunding-Background-right">
+                                <div className="friendFundingbtn"></div>
                             </div>
 
-                            </div>
+                        </Link>
+                    ))}
+
+                </div>
+
+            </div>
         </div>
 
     );
