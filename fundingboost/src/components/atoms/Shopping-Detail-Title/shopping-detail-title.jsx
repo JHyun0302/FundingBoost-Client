5f73e892952495b7af0c6473a52eb61bd881b93c
HyeIn <helen6662@naver.com>
2024-05-17 09:55:41 +0900

    [style] : scss 수정

--- fundingboost/src/components/atoms/Shopping-Detail-Title/shopping-detail-title.jsx
+++ fundingboost/src/components/atoms/Shopping-Detail-Title/shopping-detail-title.jsx
@@ -1,21 +1,26 @@
-import React from "react"
+import React from "react";
 import "./shopping-detail-title.scss";
-import img from "../../../assets/testitem.svg"
+import img from "../../../assets/testitem.svg";
 import ShoppingDetailMenu from "../Shopping-Detail-Menu/shopping-detail-menu";
 
-
-export default function ShoppingDetailItem ({ itemName, itemThumbnailImageUrl, itemPrice, option }) {
+export default function ShoppingDetailItem({ itemName, itemThumbnailImageUrl, itemPrice, option }) {
     return (
         <div className="ShoppingDetailItemBox">
             <div className="ShoppingDetailItemBoxView">
                 <div className="shopping-detail-title-wrapper">
                     <img className="image" alt="Image" src={itemThumbnailImageUrl} />
                     <div className="shopping-detail-column">
-                    <div className="itemName">{itemName}</div>
-                        <ShoppingDetailMenu itemPrice={itemPrice} option={option}/>
+                        {itemName && (
+                            <div className="itemName">
+                                {itemName.length > 40 ? itemName.slice(0, 40) : itemName}
+                                {itemName.length > 40 && <br />}
+                                {itemName.length > 40 && itemName.slice(40)}
+                            </div>
+                        )}
+                        <ShoppingDetailMenu itemPrice={itemPrice} option={option} />
                     </div>
                 </div>
             </div>
         </div>
     );
-};
+}
