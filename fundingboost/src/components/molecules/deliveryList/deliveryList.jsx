968cead1a7a2939c882e8a6712649c2e78c6e9fb
SemiHyeon <106103995+SemiHyeon@users.noreply.github.com>
2024-05-21 11:09:57 +0900

    [feat]: 마이페이지 배송지 관리 생성

---
+++ fundingboost/src/components/molecules/deliveryList/deliveryList.jsx
@@ -0,0 +1,28 @@
+import React from "react";
+import "./deliveryList.scss";
+import DeliveryAddress from "../deliveryAddress/deliveryAddress";
+
+export default function DeliveryAddressList({deliveryData}) {
+    return (
+        <div className="deliveryList">
+            <div className="deliveryAddressList-container">
+                <div className="deliveryList-box">
+                    <div className="delivery-address-list">
+                        <div className="delivery-address-list-head">
+                            <div className="delivery-address-list-delivery-management">배송지 관리</div>
+                            <button className="delivery-address-list-add-delivery">배송지 추가</button>
+                        </div>
+                        <div className="delivery-address-list-line"/>
+                        <div className="delivery-address-list-addresses">
+                            {/*{deliveryData?.data?.myPageDeliveryDtoList?.map((deliveryData, index) => (*/}
+                                <DeliveryAddress deliveryData={deliveryData}/>
+                        {/*))}*/}
+                        </div>
+                    </div>
+                </div>
+            </div>
+
+        </div>
+
+    );
+};
