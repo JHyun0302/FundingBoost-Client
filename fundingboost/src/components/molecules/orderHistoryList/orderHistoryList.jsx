2816ec931405b4a6102d3f02bcdc19c25cf35765
SemiHyeon <106103995+SemiHyeon@users.noreply.github.com>
2024-05-22 11:11:50 +0900

    [feat]: 마이페이지 구매이력 생성

--- fundingboost/src/components/molecules/orderHistoryList/orderHistoryList.jsx
+++ fundingboost/src/components/molecules/orderHistoryList/orderHistoryList.jsx
@@ -1,21 +1,32 @@
 import React from 'react';
-import DeliveryAddress from "../deliveryAddress/deliveryAddress";
+import OrderHistorySingleOrder from '../orderHistory-singleOrder/orderHistory-singleOrder';
 import './orderHistoryList.scss';
 const OrderHistoryList = () => {
     return (
         <div className="orderHistoryList">
-            <div className="orderHistoryList-contaideliveryListner">
+            <div className="orderHistoryList-container">
                 <div className="orderHistoryList-box">
                     <div className="order-history-list">
                         <div className="order-history-list-head">
-                            <div className="delivery-address-list-delivery-management">구매 이력</div>
+                            <div className="order-history-list-delivery-management">구매 이력</div>
+                            <div className="order-history-list-add-delivery">상세 조회</div>
                         </div>
-                        <div className="delivery-address-list-line"/>
-                        <div className="delivery-address-list-addresses">
+                        <div className="order-history-list-line"/>
+
+
+                            <div className="MyPageFriendFundingListOverlap-group">
+                                <div className="MyPageFriendFundingListText-wrapper-7">ITEM</div>
+                                <div className="MyPageFriendFundingListText-wrapper-6">구매한 금액</div>
+                            </div>
+
+
+
+                        <div className="order-history-list-addresses">
                             {/*{deliveryData?.data?.myPageDeliveryDtoList?.map((deliveryData, index) => (*/}
-                            <DeliveryAddress />
-                            <DeliveryAddress />
-                            <DeliveryAddress />
+                            <OrderHistorySingleOrder/>
+                            <OrderHistorySingleOrder/>
+                            <OrderHistorySingleOrder/>
+                            <OrderHistorySingleOrder/>
                             {/*))}*/}
                         </div>
                     </div>
