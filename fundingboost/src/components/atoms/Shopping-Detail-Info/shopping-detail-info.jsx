5e6bf52c72f95d5ce679283e5715c3477d5fdda0
SemiHyeon <106103995+SemiHyeon@users.noreply.github.com>
2024-06-06 23:05:54 +0900

    [style]: 쇼핑상세 페이지 상세페이지 default 이미지 추가 및 css 수정

--- fundingboost/src/components/atoms/Shopping-Detail-Info/shopping-detail-info.jsx
+++ fundingboost/src/components/atoms/Shopping-Detail-Info/shopping-detail-info.jsx
@@ -1,16 +1,27 @@
 import React from "react"
 import "./shopping-detail-info.scss";
+import shoppingTestImg from "../../../assets/shoppingDetailTest.PNG"
 
 export default function ShoppingDetailItemExplain () {
 
     return (
         <div className="box">
-            <div className="shop-item-index-container">
-                <button className="text-wrapper">상품 설명</button>
-                <button className="text-wrapper-2">리뷰</button>
-                <button className="text-wrapper-3">문의/정책</button>
+
+            <div className="shopping-detail-title">
+                <div className="shop-item-index-container">
+                    <button className="text-wrapper">상품 설명</button>
+                    <button className="text-wrapper-2">리뷰</button>
+                    <button className="text-wrapper-3">문의/정책</button>
+
+                </div>
             </div>
+            <div className="shopping-detail-img">
+                <img className="shopping-detail-testImg" src={shoppingTestImg}/>
+            </div>
+
             <div className="blank"/>
+
+
         </div>
     );
 };
